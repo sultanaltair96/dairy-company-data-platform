c87d921efbc99ef7d00fb2e5e878d1108a863c51
Taanis <tanishskarigar@gmail.com>
2026-02-13 19:49:57 +0100

    update

--- src/orchestration/assets/bronze/__init__.py
+++ src/orchestration/assets/bronze/__init__.py
@@ -4,4 +4,6 @@
 from orchestration.assets.bronze.run_bronze_example import run_bronze_example
 from orchestration.assets.bronze.run_bronze_hr_employees import run_bronze_hr_employees
 
-__all__ = ["run_bronze_example", "run_bronze_hr_employees"]
+from orchestration.assets.bronze.run_bronze_fin_general_ledger import run_bronze_fin_general_ledger
+
+__all__ = ["run_bronze_example", "run_bronze_hr_employees", "run_bronze_fin_general_ledger"]
